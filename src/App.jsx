0d1ee039dce0481360893e10803f4a4d8f5e3c44
ramazan.biyik@cutesafe.com <ramazan.biyik@cutesafe.com>
2019-08-23 16:26:02 +0300

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React from "react"; //state Kuallnıcağım için use state
+
+export default function App() {
+  return (
+    <div className="container mt-4">
+      <div className="card">
+        <div className="card-header">
+          <h4 className="mb-0">Kelime Oyununa Hoşgeldiniz</h4>
+        </div>
+        <div className="card-body">
+          <p className="mb-0">
+            Yarışmaya başlamak için Basşla butonuna tıklayınız
+          </p>
+        </div>
+        <div className="card-footer">
+          <button className="btn btn-primary">Yarışmaya Başla</button>
+        </div>
+      </div>
+      <div className="card mb-4">
+        <div className="card-header">
+          <h4 className="mb-0">Tükiyenin basşkenti neresi?</h4>
+        </div>
+        <div className="card-body">harfler</div>
+        <div className="card-footer">
+          <div class="input-group">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Cevabınız?"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+            <div class="input-group-append">
+              <button class="btn btn-outline-secondary" type="button">
+                Button
+              </button>
+              <button class="btn btn-outline-secondary" type="button">
+                Button
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "jquery";
+import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
